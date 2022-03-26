c2bedac7104703bfd169ed1e79ac2fa21538bb6a
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-26 11:13:55 -0300

    CityAccommodation

---
+++ Domain/Entities/City.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Base;
+
+namespace Domain.Entities
+{
+    public class City : BaseEntity<Guid>
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<CityAccommodation> CityAccommodations { get; set; }
+
+        public IEnumerable<Accommodation> Accommodations { get => CityAccommodations.Select(ca => ca.Accommodation); }
+    }
+}
---
+++ Domain/Entities/CityAccommodation.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Base;
+
+namespace Domain.Entities
+{
+    public class CityAccommodation : BaseEntity<int>
+    {
+        public City City { get; set; }
+        public Accommodation Accommodation { get; set; }
+    }
+}
