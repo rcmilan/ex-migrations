5532254975d311223683f99fc15563c048e1f5da
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-26 12:07:43 -0300

    cities controller

---
+++ Api/Controllers/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly IRepository<City, Guid> _repository;
+
+        public CitiesController(IRepository<City, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] City city)
+        {
+            var result = await _repository.Add(city);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var cities = _repository.GetAll();
+
+            return Ok(cities);
+        }
+    }
+}
