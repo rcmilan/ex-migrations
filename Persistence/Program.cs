730b0655f7b85a7620aae4d09feba07816f4f16f
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-26 09:13:47 -0300

    program cs

---
+++ Persistence/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Hosting;
+using Persistence;
+
+Console.WriteLine("ConnectionString:");
+string connString = Console.ReadLine();
+
+if (string.IsNullOrEmpty(connString)) throw new Exception("Connection String Vazia!");
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) => services.AddDatabaseModule(connString))
+    .Build();
+
+await host.RunAsync();
