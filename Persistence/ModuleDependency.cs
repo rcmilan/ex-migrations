26cf656d211e0401532dd3ad147b54cdf92c5027
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-26 08:50:15 -0300

    persistence

---
+++ Persistence/ModuleDependency.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence
+{
+    public static class ModuleDependency
+    {
+        public static void AddDatabaseModule(this IServiceCollection services)
+        {
+
+        }
+    }
+}
