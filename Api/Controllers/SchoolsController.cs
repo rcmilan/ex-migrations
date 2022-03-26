1acdf45dca576f08ecec86255f2e56b77a977fb8
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-26 13:35:23 -0300

    school controller

---
+++ Api/Controllers/SchoolsController.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolsController : ControllerBase
+    {
+        private readonly IRepository<School, long> _repository;
+
+        public SchoolsController(IRepository<School, long> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{id:long}")]
+        public IActionResult Get(long id)
+        {
+            var school = _repository.Get(s => s.Id == id);
+
+            return Ok(school);
+        }
+    }
+}
