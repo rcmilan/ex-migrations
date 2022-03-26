34ab96853e097b1c43806279eea574bc74d04ea7
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-26 12:49:51 -0300

    program cs

--- Persistence/Program.cs
+++ Persistence/Program.cs
@@ -2,9 +2,6 @@
 using Persistence;
 
 string connString = "server=localhost;port=3306;userid=mysqlusr;password=password;database=accommodations;";
-Console.WriteLine("ConnectionString: {0}", connString);
-
-if (string.IsNullOrEmpty(connString)) throw new Exception("Connection String Vazia!");
 
 using IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services => services.AddDatabaseModule(connString))
